51d384801b885650b54cdcdc88b5ab316ac0ad0f
SUN <sun.panlong@iwhalecloud.com>
2024-05-21 15:28:56 +0800

    modify my code

---
+++ src/main/java/com/my/code/mycode/MyCodeApplication.java
@@ -0,0 +1,13 @@
+package com.my.code.mycode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCodeApplication.class, args);
+    }
+
+}
